76ebf10d3297661e10194b296617a14a400a456e
金正朋也 <mhtnim1023@gmail.com>
2023-07-04 00:55:26 +0900

    Feat: go test

---
+++ greetings/greetings_test.go
@@ -0,0 +1,24 @@
+package greetings
+
+import (
+	"testing"
+	"regexp"
+)
+
+func TestHelloName(t *testing.T) {
+	name := "tomoya"
+	want := regexp.MustCompile(`\b`+name+`\b`)
+	msg, err := Hello("tomoya")
+
+	if !want.MatchString(name) || err != nil {
+		t.Fatalf(`Hello("Gladys") = %q, %v, want match for %#q, nil`, msg, err, want)
+	}
+
+}
+
+func TestHelloEmpty(t *testing.T) {
+	msg, err := Hello("")
+	if msg != "" || err = nil {
+		t.Fatalf(`Hello("") = %q, %v, want "", error`, msg, err)
+	}
+}

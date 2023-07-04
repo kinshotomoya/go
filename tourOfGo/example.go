0ea75396211e864219d74744ed6867c7ea822715
金正朋也 <mhtnim1023@gmail.com>
2023-07-04 23:45:47 +0900

    Feat: if

--- tourOfGo/example.go
+++ tourOfGo/example.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func main() {
 	basicType()
@@ -8,6 +11,10 @@ func main() {
 	typeInterface()
 	constant()
 	constantNumber()
+	forstate()
+	for2()
+	for3()
+	ifWithShortStat()
 }
 
 func basicType() {
@@ -54,7 +61,44 @@ func constantNumber() {
 	const Big = 1 << 60
 	fmt.Printf("type is %T. value is %v", Big, Big)
 
-	const Small float64 = 1 << 60
-	// TODO: 浮動小数点の理解
+	const Small float64 = 1 << 100
+	// 浮動小数点
+	// 指数部・仮数部の2進数で表現することで、int64よりも大きな数を表現できる
 	fmt.Printf("\ntype is %T. value is %v", Small, Small)
+}
+
+func forstate() {
+	sum := 0
+	for i := 0; i < 10; i++ {
+		sum += 1
+	}
+	fmt.Println("\n", sum)
+}
+
+func for2() {
+	sum := 1
+	for ; sum< 1000; {
+		sum += sum
+	}
+	fmt.Println("\n", sum)
+}
+
+func for3() {
+	sum := 1
+	for sum < 1000 {
+		sum += sum
+	}
+	fmt.Println("\n", sum)
+}
+
+func ifWithShortStat() {
+	var x, y, lim float64 = 3, 2, 10
+	// if文に変数を定義できる
+	// これはifブロック内でのみ利用できる
+	if v := math.Pow(x, y); v < lim {
+		fmt.Println(v)
+	} else {
+		// else内でも使える
+		fmt.Println(v)
+	}
 }

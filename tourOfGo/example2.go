fcc659beb3d5c84e219108ce37f15aacac0046e6
金正朋也 <mhtnim1023@gmail.com>
2023-07-05 00:52:22 +0900

    Feat: メモリ管理に関しての参考メモ

---
+++ tourOfGo/example2.go
@@ -0,0 +1,13 @@
+package main
+
+func main() {
+
+}
+
+func pointer() {
+	// 続き
+	// https://go-tour-jp.appspot.com/moretypes/1
+}
+
+// NOTE: goのメモリ管理に関しては↓参照
+//https://zenn.dev/kazu1029/articles/38ab3d99ef0de3

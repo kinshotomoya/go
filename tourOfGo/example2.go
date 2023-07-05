83557a9de4c13ea806c0512a3239fd381c71ae79
金正朋也 <mhtnim1023@gmail.com>
2023-07-05 19:01:25 +0900

    Feat: struct

--- tourOfGo/example2.go
+++ tourOfGo/example2.go
@@ -1,13 +1,44 @@
 package main
 
-func main() {
+import (
+	"fmt"
+)
 
+func main() {
+	pointer()
+	structs()
 }
 
 func pointer() {
 	// 続き
 	// https://go-tour-jp.appspot.com/moretypes/1
+	i := 1
+	p := &i
+
+	fmt.Println(p)  // メモリアドレス
+	fmt.Println(*p) // 実体
+
+	*p = 100
+	fmt.Println(p)  // メモリアドレス
+	fmt.Println(*p) // 実体
+
 }
 
 // NOTE: goのメモリ管理に関しては↓参照
 //https://zenn.dev/kazu1029/articles/38ab3d99ef0de3
+
+func structs() {
+
+	type Person struct {
+		name string
+		age  int
+	}
+
+	person := Person{"javen", 1}
+	fmt.Println(person.name)
+
+	person2 := Person{"javen", 100}
+	p := &person2
+	fmt.Println(p)
+
+}
